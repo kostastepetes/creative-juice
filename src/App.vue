75bdc7b038d2b292b76e7a8eb1a8a7277c1f8b92
kostastepetes <st1064973@ceid.upatras.gr>
2023-11-06 19:37:13 +0200

    login + register + auth

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,48 @@
-<script setup>
-import { supabase } from "./supabase"
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- Check if user is available in the store, if not show auth compoenent -->
+  <Auth v-if="!store.state.user" />
+  <!-- If user is available, show the Hello World component -->
+  <HelloWorld v-else msg="Hello Vue 3 + Vite" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+import Auth from "./components/Auth.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+import { store } from "./store";
+import { supabase } from "./supabase";
+
+export default {
+  components: {
+    HelloWorld,
+    Auth,
+  },
+  setup() {
+    // we initially verify if a user is logged in with Supabase
+    store.state.user = supabase.auth.getUser()?.user;
+    // we then set up a listener to update the store when the user changes either by logging in or out
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event == "SIGNED_OUT") {
+        store.state.user = null;
+      } else {
+        store.state.user = session.user;
+      }
+    });
+
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
