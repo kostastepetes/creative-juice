babc403aae3715b45f3165f13a67efca9f8a8070
kostastepetes <st1064973@ceid.upatras.gr>
2023-11-07 21:29:30 +0200

    account + avatar

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,25 @@
-<template>
-  <!-- Check if user is available in the store, if not show auth compoenent -->
-  <Auth v-if="!store.state.user" />
-  <!-- If user is available, show the Hello World component -->
-  <HelloWorld v-else msg="Hello Vue 3 + Vite" />
-</template>
+<script setup>
+import { onMounted, ref } from 'vue'
+import Account from './components/Account.vue'
+import Auth from './components/Auth.vue'
+import { supabase } from './supabase'
 
-<script>
-import Auth from "./components/Auth.vue";
-import HelloWorld from "./components/HelloWorld.vue";
+const session = ref()
 
-import { store } from "./store";
-import { supabase } from "./supabase";
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
 
-export default {
-  components: {
-    HelloWorld,
-    Auth,
-  },
-  setup() {
-    // we initially verify if a user is logged in with Supabase
-    store.state.user = supabase.auth.getUser()?.user;
-    // we then set up a listener to update the store when the user changes either by logging in or out
-    supabase.auth.onAuthStateChange((event, session) => {
-      if (event == "SIGNED_OUT") {
-        store.state.user = null;
-      } else {
-        store.state.user = session.user;
-      }
-    });
-
-    return {
-      store,
-    };
-  },
-};
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="session" :session="session" />
+    <Auth v-else />
+  </div>
+</template>
---
+++ src/components/Account.vue
@@ -0,0 +1,108 @@
+<script setup>
+import { supabase } from '../supabase'
+import Avatar from './Avatar.vue'
+import { onMounted, ref, toRefs } from 'vue'
+
+const props = defineProps(['session'])
+const { session } = toRefs(props)
+
+const loading = ref(true)
+const username = ref('')
+const contact_info = ref('')
+const avatar_url = ref('')
+
+onMounted(() => {
+  getProfile()
+})
+
+async function getProfile() {
+  try {
+    loading.value = true
+    const { user } = session.value
+
+    const { data, error, status } = await supabase
+      .from('Profiles')
+      .select(`username, contact_info, avatar_url`)
+      .eq('id', user.id)
+      .single()
+
+    if (error && status !== 406) throw error
+
+    if (data) {
+      username.value = data.username
+      contact_info.value = data.contact_info
+      avatar_url.value = data.avatar_url
+    }
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+
+async function updateProfile() {
+  try {
+    loading.value = true
+    const { user } = session.value
+
+    const updates = {
+      id: user.id,
+      username: username.value,
+      contact_info: contact_info.value,
+      avatar_url: avatar_url.value,
+      updated_at: new Date(),
+    }
+
+    const { error } = await supabase.from('Profiles').upsert(updates)
+
+    if (error) throw error
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+
+async function signOut() {
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signOut()
+    if (error) throw error
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+  <form class="form-widget" @submit.prevent="updateProfile">
+    <Avatar v-model:path="avatar_url" @upload="updateProfile" size="10" />
+    <div>
+      <label for="email">Email</label>
+      <input id="email" type="text" :value="session.user.email" disabled />
+    </div>
+    <div>
+      <label for="username">Name</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="contact_info">Contact Info - Website or Social URL</label>
+      <input id="contact_info" type="url" v-model="contact_info" />
+    </div>
+
+    <div>
+      <input
+        type="submit"
+        class="button primary block"
+        :value="loading ? 'Loading ...' : 'Update'"
+        :disabled="loading"
+      />
+    </div>
+
+    <div>
+      <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
+    </div>
+  </form>
+</template>
