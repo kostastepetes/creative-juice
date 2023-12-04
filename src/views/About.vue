5da03f68e2ac4d144f93ced0d5fbf1607d54e2b3
kostastepetes <st1064973@ceid.upatras.gr>
2023-12-04 13:04:40 +0200

    gsap animations

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,28 +2,28 @@
     <Navbar :session="session"/>
     <div class="container py-4">
       <div class="row mt-4 mb-4">
-        <div class="col-lg-6">
+        <div class="col-lg-6" ref="col1">
           <img src="..\assets\Creative_Juice_Logo_4-removebg-preview.png" class="img-fluid" alt="Responsive image">
         </div>
-        <div class="col-lg-6 d-flex flex-column justify-content-center">
+        <div class="col-lg-6 d-flex flex-column justify-content-center" ref="col2">
           <h1>Our Vision</h1>
           <p class="mt-2">Our vision is to create a community of creative people and enable an idea-sharing and knowledge exchange environment where different creatives can network with each other online or face-to-face over coffee.</p>
         </div>
       </div>
       <div class="row mt-4 mb-4">
-        <div class="col-lg-6 d-flex flex-column justify-content-center">
+        <div class="col-lg-6 d-flex flex-column justify-content-center"  ref="col3">
           <h1>Creative Networking</h1>
           <p class="mt-2">Our members gain the ability to feel accepted and a valuable part of a broad community. They create strong relationships with like-minded people and find new mentors or collaborators.</p>
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-6"  ref="col4">
           <img src="..\assets\undraw_conversation_re_c26v.svg" class="img-fluid" alt="Responsive image">
         </div>
       </div>
       <div class="row mt-4 mb-4">
-        <div class="col-lg-6">
+        <div class="col-lg-6"  ref="col5">
           <img src="..\assets\undraw_hang_out_re_udl4.svg" class="img-fluid" alt="Responsive image">
         </div>
-        <div class="col-lg-6 d-flex flex-column justify-content-center">
+        <div class="col-lg-6 d-flex flex-column justify-content-center" ref="col6">
           <h1>Knowledge Transfer</h1>
           <p class="mt-2">Our members will be able to participate into an unlimited transfer of a wide variety of info and tips and become a link into a stable chain of knowledge and inspiration.</p>
         </div>
@@ -34,8 +34,9 @@
   </template>
   
   <script>
-  import { onMounted, ref } from "vue";
+  import { onMounted, ref, nextTick } from "vue";
   import { supabase } from "../supabase";
+  import { gsap } from "gsap";
   import Navbar from '../components/Navbar.vue';
   import Footer from '../components/Footer.vue';
   
@@ -43,7 +44,13 @@
    name: 'About',
    components: { Navbar, Footer },
    setup() {
-     const session = ref(null);
+    const session = ref(null);
+    const col1 = ref(null);
+    const col2 = ref(null);
+    const col3 = ref(null);
+    const col4 = ref(null);
+    const col5 = ref(null);
+    const col6 = ref(null);
   
      onMounted(() => {
        supabase.auth.getSession().then(({ data }) => {
@@ -53,9 +60,18 @@
        supabase.auth.onAuthStateChange((_, _session) => {
          session.value = _session;
        });
-     });
+
+       nextTick().then(() => {
+      gsap.from(col1.value, { x: "-100%", duration: 1 });
+      gsap.from(col2.value, { x: "100%", duration: 1 });
+      gsap.from(col3.value, { x: "-100%", duration: 1 });
+      gsap.from(col4.value, { x: "100%", duration: 1 });
+      gsap.from(col5.value, { x: "-100%", duration: 1 });
+      gsap.from(col6.value, { x: "100%", duration: 1 });
+      });
+    });
   
-     return { session };
+    return { session, col1, col2, col3, col4, col5, col6 };
    },
   };
   </script>
